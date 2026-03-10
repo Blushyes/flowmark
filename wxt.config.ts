3f4243a5159d8edf4b0cb4191b41ad300c313353
Blushyes <Blushyes@gmail.com>
2026-03-10 17:49:37 +0800

    reuse profile file for dev environment

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,9 +1,15 @@
+import { resolve } from 'node:path';
+
 import { defineConfig } from 'wxt';
 import tailwindcss from '@tailwindcss/vite';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-solid'],
+  webExt: {
+    chromiumProfile: resolve('.wxt/chromium-profile'),
+    keepProfileChanges: true,
+  },
   vite: () => ({
     plugins: [tailwindcss()],
   }),
